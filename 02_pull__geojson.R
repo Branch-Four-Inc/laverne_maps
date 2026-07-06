ca6745db94a92d8ceb309fa01d2a2b1f73049b99
Mieko Chun <165838850+m1chun@users.noreply.github.com>
2026-07-06 14:51:08 -0700

    Update shapefiles code and add geojson files for La Verne

--- 02_pull__geojson.R
+++ 02_pull__geojson.R
@@ -1,8 +1,7 @@
 # -----------------------------------------------------------------------
-# Name: Hudson County and NYC GeoJSON Pull
-# Author: Sami Smalling
+# Name: La Verne GeoJSON Pull
+# Updated by: Mieko Chun
 # -----------------------------------------------------------------------
-
 
 library(tigris)
 library(sf)
@@ -10,34 +9,29 @@ library(dplyr)
 
 options(tigris_use_cache = TRUE)
 
-# pull boundaries for New York and filter for NYC counties
-nyc_counties <- counties(state = "NY", cb = TRUE) |>
-    filter(COUNTYFP %in% c("061", "047", "081", "005", "085"))
+# pull tract boundaries for LA County and filter for La Verne tracts
+lv_tracts <- tracts(state = "CA", county = "Los Angeles", year = 2020) |>
+    filter(NAME %in% c("4002.07", "4002.08", "4002.09", "4015", "4016.01", "4016.02", "4089"))
 
-
-# pull boundaries for New Jersey and filter for Hudson County (FIPS: 017)
-nj_hudson <- counties(state = "NJ", cb = TRUE) |>
-    filter(COUNTYFP == "017")
-
-# combine the counties into a single dataset, keep ID and NAME
-final_map_data <- rbind(nyc_counties, nj_hudson) |>
+# combine the tracts into a single dataset, keep ID and NAME
+final_map_data <- lv_tracts |>
     select(GEOID, NAME)
 
 
 # convert coordinates to  standard WGS84 (lat/long), which Leaflet expects
 final_map_data <- st_transform(final_map_data, crs = 4326)
 
-# write to boundaries.geojson file
+# write to tract boundaries.geojson file
 st_write(
     final_map_data,
-    "sliceofculture/growth_map/housing_growth_map/county_boundaries.geojson",
+    "housing_maps/lv_housing_county_boundaries.geojson",
     driver = "GeoJSON",
     delete_dsn = TRUE
 )
 
 st_write(
     final_map_data,
-    "sliceofculture/growth_map/demographic_growth_map/county_boundaries.geojson",
+    "housing_maps/lv_population_county_boundaries.geojson",
     driver = "GeoJSON",
     delete_dsn = TRUE
 )
